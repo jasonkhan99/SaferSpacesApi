29813c1f43ee76ebd95a10318ec8d2e83fac3ac4
MarielHamson <45244997+MarielHamson@users.noreply.github.com>
2020-08-24 15:51:53 -0700

    add new migration
    Co-authored-by: Jason Khan <jasonderekkhan@gmail.com>
    Co-authored-by: RichterBros <erich.richter@gmail.com>
    Co-authored-by: Britt <brittanylindgren2010@gmail.com>

--- SaferSpacesApi/Migrations/20200824222337_NewJoins.Designer.cs
+++ SaferSpacesApi/Migrations/20200824222337_NewJoins.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SaferSpacesApi.Models;
 
 namespace SaferSpacesApi.Migrations
 {
     [DbContext(typeof(SaferSpacesApiContext))]
-    partial class SaferSpacesApiContextModelSnapshot : ModelSnapshot
+    [Migration("20200824222337_NewJoins")]
+    partial class NewJoins
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
